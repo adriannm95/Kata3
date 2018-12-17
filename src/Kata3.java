a84d45db661347c7720dbe71d7ef781635c5e533
Adrián <adriannegrinmartin95@gmail.com>
2018-12-17 20:44:02 +0000

    Kata 3 Version 2

--- src/Kata3.java
+++ src/Kata3.java
@@ -1,7 +1,45 @@
 public class Kata3 {
 
     public static void main(String[] args) {
-        HistogramDisplay histo = new HistogramDisplay();
-        histo.execute();
+
+        Histogram<String> histogram = new Histogram<>();
+
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("hotmail.com");
+        histogram.increment("hotmail.com");
+        histogram.increment("hotmail.com");
+        histogram.increment("hotmail.com");
+        histogram.increment("hotmail.com");
+        histogram.increment("hotmail.com");
+        histogram.increment("ull.es");
+        histogram.increment("ull.es");
+        histogram.increment("ull.es");
+        histogram.increment("ull.es");
+        histogram.increment("ull.es");
+        histogram.increment("ull.es");
+        histogram.increment("ull.es");
+        histogram.increment("ull.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+
+        new HistogramDisplay(histogram).execute();
     }
 }
